ce6cfd4378e1b2eb8377b7f60095684da303ad05
RaushanKumar25 <raushankumarbhardwaj4510@gmail.com>
2025-06-19 13:42:36 +0530

    Implemented header navigation (Home, Capabilities, Projects, About) and linked social icons to respective social media profiles

---
+++ img/app.js
@@ -0,0 +1,7 @@
+const hamb = document.querySelector('.header .nav-bar .nav-list .hamb');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list .ul');
+const header = document.querySelector('.header .container');
+
+hamb.addEventListener('click', () => {
+    hamb.classList.toggle('active');
+});
